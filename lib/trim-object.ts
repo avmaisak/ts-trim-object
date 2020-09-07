04fae5a8b50549e640f045edb6ccd4cdb7f97a8a
Anton Maisak <anton@maisak.ru>
2020-09-07 21:17:34 +0300

    added object extension method fixed test pkg updated

--- lib/trim-object.ts
+++ lib/trim-object.ts
@@ -9,20 +9,18 @@ import { ObjectType } from "./objtype.enum";
  */
 export class TrimObject {
 
-  static hasOnlySpaces(str: string): boolean {
+  /** this helper checks, that string has only white spaces. */
+  private static hasOnlySpaces(str: string): boolean {
     if (!str) return false;
     if (str.length === 0) return false;
-
     for (let index = 0; index < str.length; index++) if (str[index] !== ' ') return false;
-
     return true;
   }
 
   /**
    * Remove empty properties form object.
-   * @param obj 
    */
-  static trimProps(obj: any, returnNullIfEmpty = false): any {
+  static trimProps(obj: any): any {
 
     Object.keys(obj).forEach((key) => {
 
@@ -30,26 +28,37 @@ export class TrimObject {
 
       if (value === null || value === '') {
         delete obj[key];
+        return obj;
       }
-      else {
-        switch (value.constructor.name) {
-          case ObjectType.Object: obj[key] = this.trimProps(value as any); break;
-          case ObjectType.String: {
-            if (this.hasOnlySpaces(value)) delete obj[key];
-            if (obj[key])
-              obj[key] = value.trim(); break;
-          }
-          case ObjectType.Array:
-            const arr = value as Array<any>;
-            if (!arr || arr.length === 0) delete obj[key];
-            for (let index = 0; index < arr.length; index++) arr[index] = this.trimProps(arr[index]);
-            break;
-          default: break;
+
+      switch (value.constructor.name) {
+        case ObjectType.Object: obj[key] = this.trimProps(value as any); break;
+        case ObjectType.String: {
+          if (this.hasOnlySpaces(value)) delete obj[key];
+          if (obj[key]) obj[key] = value.trim(); break;
         }
+        case ObjectType.Array:
+          const arr = value as Array<any>;
+          if (!arr || arr.length === 0) delete obj[key];
+          for (let index = 0; index < arr.length; index++) arr[index] = this.trimProps(arr[index]);
+          break;
+        default: break;
       }
     });
 
-    if (returnNullIfEmpty && (!Object.keys(obj) || Object.keys(obj).length === 0)) return null;
     return obj;
   }
+}
+
+/** Declaring Global Extension method. */
+declare global {
+  interface Object {
+    /** Remove empty properties form object. */
+    trimProps: () => any;
+  }
+}
+
+/** Remove empty properties form object. */
+Object.prototype.trimProps = function (): any {
+  return TrimObject.trimProps(this);
 }
--- tests/trim-object.spec.ts
+++ tests/trim-object.spec.ts
@@ -1,138 +1,76 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { assert, expect } from 'chai';
 import 'mocha';
-import { TrimObject } from '../lib/trim-object';
+import '../lib/trim-object';
 
 describe('Main test', () => {
 
-  it('Helper - only spaces. Test 1 ', () => {
-    const str = '                                   ';
-    const actual = TrimObject.hasOnlySpaces(str);
-    expect(true).equal(actual);
+  it('Simple object check ', () => {
+    const obj = { item: 'TestName' };
+    const actual = obj.trimProps();
+    expect(obj === actual).equal(true);
   });
-
-  it('Helper - only spaces. Test 2 ', () => {
-    const str = '               as      adasd             as  ';
-    const actual = TrimObject.hasOnlySpaces(str);
-    expect(false).equal(actual);
-  });
-
-  it('Helper - only spaces. Test 3 ', () => {
-    const str = 'test';
-    const actual = TrimObject.hasOnlySpaces(str);
-    expect(false).equal(actual);
-  });
-
-  it('Helper - only spaces. Test 4 ', () => {
-    const str = ' test';
-    const actual = TrimObject.hasOnlySpaces(str);
-    expect(false).equal(actual);
-  });
-
-  it('Helper - only spaces. Test 5 ', () => {
-    const str = ' test     ';
-    const actual = TrimObject.hasOnlySpaces(str);
-    expect(false).equal(actual);
-  });
-
-  it('Helper - only spaces. Test 6 ', () => {
-    const str = ' ';
-    const actual = TrimObject.hasOnlySpaces(str);
-    expect(true).equal(actual);
-  });
-
 
   it('Simple object check ', () => {
     const obj = { item: 'TestName' };
-    const actual = TrimObject.trimProps(obj);
+    const actual = obj.trimProps();
     expect(obj === actual).equal(true);
   });
 
   it('Simple object check (trim)', () => {
     const obj = { item: '        ', field: 'field' };
-    const actual = TrimObject.trimProps(obj);
+    const actual = obj.trimProps();
     const expected = { field: 'field' };
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
 
   it('Simple object check with trim left - right', () => {
     const obj = { item: '             TestName               ' };
-    const actual = JSON.stringify(TrimObject.trimProps(obj));
+    const actual = JSON.stringify(obj.trimProps());
     const expected = JSON.stringify({ item: 'TestName' });
     expect(expected === actual).equal(true);
   });
 
   it('Object check (with array)', () => {
-    const obj = { item: 'TestName    ', item2: '', products: [{ name: 'Product1', price: 3, location: '' }] } as any;
-    const actual = TrimObject.trimProps(obj);
+    const obj = { item: 'TestName    ', item2: '', products: [{ name: 'Product1', price: 3, location: '' }] };
+    const actual = obj.trimProps();
     const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3 }] };
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
 
   it('Object check (with nested array)', () => {
-    const obj = { item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: 3, location: '' }] }] } as any;
-    const actual = TrimObject.trimProps(obj);
+    const obj = { item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: 3, location: '' }] }] };
+    const actual = obj.trimProps();
     const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3, items: [{ name: 'Product1', price: 3 }] }] };
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
 
   it('Object check (with nested array)', () => {
-    const obj = { test: null, item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: null, location: '' }] }] } as any;
-    const actual = TrimObject.trimProps(obj);
+    const obj = { test: null, item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: null, location: '' }] }] };
+    const actual = obj.trimProps();
     const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3, items: [{ name: 'Product1' }] }] };
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
 
   it('Object check (with nested array)', () => {
-    const obj = { test: Object, item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: null, location: '' }] }] } as any;
-    const actual = TrimObject.trimProps(obj);
+    const obj = { test: Object, item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: null, location: '' }] }] };
+    const actual = obj.trimProps();
     const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3, items: [{ name: 'Product1' }] }] };
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
 
   it('Object check (with nested nullable array and Date type)', () => {
     const d = new Date();
-    const obj = { test: Object, date: d, items: [] } as any;
-    const actual = TrimObject.trimProps(obj);
+    const obj = { test: Object, date: d, items: [] };
+    const actual = obj.trimProps();
     const expected = { date: d };
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
 
   it('Object check nullable', () => {
-    const obj = { name: null } as any;
-    const actual = TrimObject.trimProps(obj);
+    const obj = { name: null };
+    const actual = obj.trimProps();
     const expected = {};
     assert.equal(JSON.stringify(actual), JSON.stringify(expected));
   });
-
-  it('Object check nullable [returnNullIfEmpty]', () => {
-    const obj = { name: null } as any;
-    const actual = TrimObject.trimProps(obj, true);
-    const expected = null;
-    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
-  });
-
-
-  it('Object check nullable nested object', () => {
-    const obj = { name: 'nested Object', object: { name: '' } } as any;
-    const actual = TrimObject.trimProps(obj, true);
-    const expected = JSON.stringify({ name: 'nested Object', object: {} } as any);
-    assert.equal(JSON.stringify(actual), expected);
-  });
-
-  it('Object check nullable nested object with array', () => {
-    const obj = { name: 'nested Object', object: { name: '', arr: [{ name1: 'name name' }] } } as any;
-    const actual = TrimObject.trimProps(obj, true);
-    const expected = JSON.stringify({ name: 'nested Object', object: { arr: [{ name1: 'name name' }] } } as any);
-    assert.equal(JSON.stringify(actual), expected);
-  });
-
-
-  it('Object check boolean', () => {
-    const obj = { name: null, deleted: false } as any;
-    const actual = TrimObject.trimProps(obj, true);
-    const expected = { deleted: false };
-    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
-  });
-
 });
