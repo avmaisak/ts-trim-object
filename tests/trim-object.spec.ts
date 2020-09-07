c19cfea89f465db2f5931a1f15bb52c91b905268
Anton Maisak <anton@maisak.ru>
2020-09-07 12:22:15 +0300

    first version. added Tests added Linter

---
+++ tests/trim-object.spec.ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { assert, expect } from 'chai';
+import 'mocha';
+import { TrimObject } from '../lib/trim-object';
+
+describe('Main test', () => {
+
+  it('Simple object check ', () => {
+    const obj = { item: 'TestName' };
+    const actual = TrimObject.trimProps(obj);
+    expect(obj === actual).equal(true);
+  });
+
+
+
+  it('Simple object check with trim left - right', () => {
+    const obj = { item: '             TestName               ' };
+    const actual = JSON.stringify(TrimObject.trimProps(obj));
+    const expected = JSON.stringify({ item: 'TestName' });
+    expect(expected === actual).equal(true);
+  });
+
+  it('Object check (with array)', () => {
+    const obj = { item: 'TestName    ', item2: '', products: [{ name: 'Product1', price: 3, location: '' }] } as any;
+    const actual = TrimObject.trimProps(obj);
+    const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3 }] };
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+  it('Object check (with nested array)', () => {
+    const obj = { item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: 3, location: '' }] }] } as any;
+    const actual = TrimObject.trimProps(obj);
+    const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3, items: [{ name: 'Product1', price: 3 }] }] };
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+
+  it('Object check (with nested array)', () => {
+    const obj = { test: null, item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: null, location: '' }] }] } as any;
+    const actual = TrimObject.trimProps(obj);
+    const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3, items: [{ name: 'Product1' }] }] };
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+  it('Object check (with nested array)', () => {
+    const obj = { test: Object, item: 'TestName    ', item2: '', products: [{ name: 'Product1                  ', price: 3, location: '', items: [{ name: 'Product1', price: null, location: '' }] }] } as any;
+    const actual = TrimObject.trimProps(obj);
+    const expected = { item: 'TestName', products: [{ name: 'Product1', price: 3, items: [{ name: 'Product1' }] }] };
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+  it('Object check (with nested nullable array and Date type)', () => {
+    const d = new Date();
+    const obj = { test: Object, date: d, items: [] } as any;
+    const actual = TrimObject.trimProps(obj);
+    const expected = { date: d };
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+  it('Object check nullable', () => {
+    const obj = { name: null } as any;
+    const actual = TrimObject.trimProps(obj);
+    const expected = {};
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+  it('Object check nullable [returnNullIfEmpty]', () => {
+    const obj = { name: null } as any;
+    const actual = TrimObject.trimProps(obj, true);
+    const expected = null;
+    assert.equal(JSON.stringify(actual), JSON.stringify(expected));
+  });
+
+});
